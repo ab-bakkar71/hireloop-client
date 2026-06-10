1062c5ed3dd2671b7fd06650c88383f04e189be2
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-06-10 20:58:04 +0600

    initeal setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,14 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="dark"
+      className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body
+      suppressHydrationWarning
+      className="min-h-full flex flex-col">
+        {children}
+        </body>
     </html>
   );
 }
